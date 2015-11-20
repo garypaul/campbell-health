c1019b8a0e49ab7948be98bd600ec00fceb17a1d
Gary Armstrong <garypaul@gmail.com>
2015-11-20 10:25:13 -0800

    Added sorting to Team Posts
    Team Members now sorted ascenting by menu_order, folllowed by title. Also code cleanup to remove redundant <?php ?>

--- CampbellHealth/content-team.php
+++ CampbellHealth/content-team.php
@@ -1,7 +1,19 @@
-<?php $team_members = get_posts(array('post_type' => 'team_members')) ?>
+<?php 
+	$args = array( 'post_type'	=> 'team_members'
+		// 'posts_per_page'   => 5,
+		// 'offset'           => 0,
+		// 'category'         => '',
+		// 'category_name'    => '',
+		 ,'orderby'	=> 'menu_order title'
+		 ,'order'	=> 'ASC' );
+	
+	$team_members = get_posts( $args ); 
 
-		<?php foreach ($team_members as $post) 
-			: setup_postdata($post) ?>
-			<?php get_template_part( 'content', 'team_member' ); ?>
-			
-		<?php endforeach; wp_reset_postdata() ?>
+foreach ($team_members as $post) 
+	: setup_postdata($post);
+
+	get_template_part( 'content', 'team_member' );
+
+endforeach; wp_reset_postdata() 
+
+?>
